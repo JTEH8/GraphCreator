2b0fcb405770b1deeb8f29179a5bff8bd5f03e28
JTEH8 <jeffteh22@gmail.com>
2022-06-12 22:38:31 -0700

    Added AddVertex and RemoveVertex

--- Main.cpp
+++ Main.cpp
@@ -2,33 +2,117 @@
 #include <cstring>
 #include "Node.h"
 
-void addNode(char newName, char* vertices[20]);
-void PRINT(char* vertices[20], int* edges[20][20]);
+
+Node* findNode(Node** list, char input, int indexCount);
+void addNode(char newName, Node **list, int indexCount);
+void addEdge(Node ** list, int indexCount);
+void PRINT(Node **list, int indexCount);
 void PATH(char firstNode, char secondNode, char* vertices[20], int* edges[20][20]);
+void deleteNode(char name, Node **list, int indexCount);
+void deleteEdge(Node **list, int indexCount);
 
 using namespace std;
 
 int main(){
 Node **list = new Node*[20];
 int IndexCounter = 0;
-for(int i = 0; i < 20; i++){
-arr2[i] = '\0';
-}
+
 char input[100];
 bool running = true;
+cout << "Welcome to Graph Creator!" << endl;
 while(running == true){
-cout << "Welcome to Graph Creator! To add a vertex, type 'ADD'. To search for the shortest path type 'PATH'. To print, type 'PRINT'." << endl;
+cout << "To add a vertex, type 'VADD'. To add an edge, type 'EADD'. " << endl;
+cout << "To remove a vertex type 'VREMOVE'. To remove an edge, type 'EREMOVE'. " << endl;
+cout << "To search for the shortest path type 'PATH'. To print, type 'PRINT'." << endl;
+cout << "To quit type QUIT." << endl;
 cin >> input;
-if(strcmp(input, "ADD") == 0){
-    char nodeName;
-    int vertexLength = 0;
-    cout << "What do you want to call the new node?" << endl;
-    cin >> nodeName;
-    addNode(nodeName, arr2);
+if(strcmp(input, "ADDV") == 0){
+    char temp;
+    cout << "Enter the  character name for the new vertex." << endl;
+    cin >> temp; 
+    addNode(temp, list, IndexCounter);
+    IndexCounter++;
     }
-if(strcmp(input, "PATH") == 0){
+else if(strcmp(input, "DELETEV")){
+    char name;
+    cout << "What is the name of the vertex you want to delete?" << endl;
+    cin >> name;
+    deleteNode(name, list, IndexCounter);
+    IndexCounter--;
+}
+else if(strcmp(input, "PRINT")){
+    PRINT(list, IndexCounter);
+    cout << endl;
+}
+else if(strcmp(input, "PATH") == 0){
     char firstNode;
-    
 }
+else if(strcmp(input, "QUIT")){
+    cout << "Thanks for using graph creator!" << endl;
+    running = false;
 }
+else{
+    cout << "Invalid input, please enter something else." << endl;
+        }
+    }
+}
+
+void addNode(char newName, Node **list, int indexCount){
+    //New Vertexr
+    Node* newVertex = new Node();
+    //Add in all it's info, make sure it's index and name are correct
+    newVertex->setIndex(indexCount);
+    newVertex->setName(newName);
+    //Add it to the list
+    list[indexCount] = newVertex;
+    cout << endl;
+    cout << "Vertex " << newVertex->getName() << " added." << endl;
+}
+
+void removeNode(char name, Node **list, int indexCount){
+    if(list[0] == NULL){
+        cout << endl;
+        cout << "There's no vertices currently on the graph! Add some first." << endl;
+        return;
+    }
+    Node* vertex = findNode(list, name, indexCount);
+    if(vertex == NULL){
+        cout << endl;
+        cout << "No vertex found. Try another one!" << endl;
+        return;
+    }
+    else{
+        int x = vertex->getIndex();
+        //Remove data from the vertex
+        vertex->~Node(); 
+        cout << "The vertex at index " << x << " has been removed." << endl;
+        //Remake the edge list
+        for(int i = 0; i < indexCount; i++){
+            Node* current = list[i];
+            for(int j = 1; j < indexCount; j++){
+                //If you get to where the current edge is one less than the index
+                if(j == (indexCount - 1)){
+                    current->setEdge(j, 0);
+                }
+                //Otherwise move all the edges above down by 1 index.
+                else{
+                    int next  = j + 1;
+                    current->setEdge(j, current->getEdge(next));
+                }
+            }
+            list[i] = current;
+        }
+        list[x] = NULL;
+        while(x < indexCount){
+            if(x = (indexCount - 1)){
+                list[x] = NULL;
+                break;
+            }
+            list[x] = list[x+1];
+            list[x]->setIndex(x);
+            x++;
+        }
+    cout << endl;
+    cout << "Vertex has been deleted" << endl;
+    }
 }
